a57681d7f37826cad6edc28c31389ddceae7e100
jlearns9 <142127358+jlearns9@users.noreply.github.com>
2023-09-10 13:32:56 -0500

    uPdate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const convertBtn = document.getElementById("convert-btn")
 const btmEl = document.getElementById("btm-el")
+const errorMsg = document.getElementById("error-msg")
 
 const conversionInfo = [
     {
@@ -22,26 +23,37 @@ const conversionInfo = [
     }
 ]
 
-for (let i = 0; i < conversionInfo.length; i++) {
-    btmEl.innerHTML += `
-    <div class="conversion-container">
-        <h3>${conversionInfo[i].title}</h3>
-        <p>${conversionInfo[i].measurement1} to ${conversionInfo[i].measurement2} | ${conversionInfo[i].measurement2} to ${conversionInfo[i].measurement1}</p>
-    </div>
-    `
-}
-
-convertBtn.addEventListener("click", function() {
+function displayConversion() {
     btmEl.innerHTML = " "
-    const inputValue = parseInt(document.getElementById("input-value").value)
     for (let i = 0; i < conversionInfo.length; i++) {
-        const conv1 = (inputValue * conversionInfo[i].conversionValue).toFixed(3)
-        const conv2 = (inputValue / conversionInfo[i].conversionValue).toFixed(3)
         btmEl.innerHTML += `
         <div class="conversion-container">
             <h3>${conversionInfo[i].title}</h3>
-            <p>${inputValue} ${conversionInfo[i].measurement1} = ${conv1} ${conversionInfo[i].measurement2} | ${inputValue} ${conversionInfo[i].measurement2} = ${conv2} ${conversionInfo[i].measurement1}</p>
+            <p id="conversionTextStyle">${conversionInfo[i].measurement1} to ${conversionInfo[i].measurement2} | ${conversionInfo[i].measurement2} to ${conversionInfo[i].measurement1}</p>
         </div>
         `
+    }    
+}
+
+displayConversion()
+
+convertBtn.addEventListener("click", function() {
+    btmEl.innerHTML = " "
+    const inputValue = document.getElementById("input-value").valueAsNumber
+    if (isNaN(inputValue)) {
+        errorMsg.innerHTML = "<p>Invalid input. Please enter a number.</p>";
+        displayConversion();
+    } else {
+        errorMsg.innerHTML = "ㅤ"
+        for (let i = 0; i < conversionInfo.length; i++) {
+            const conv1 = (inputValue * conversionInfo[i].conversionValue).toFixed(3)
+            const conv2 = (inputValue / conversionInfo[i].conversionValue).toFixed(3)
+            btmEl.innerHTML += `
+            <div class="conversion-container">
+                <h3>${conversionInfo[i].title}</h3>
+                <p id="conversionTextStyle">${inputValue} ${conversionInfo[i].measurement1} = ${conv1} ${conversionInfo[i].measurement2} | ${inputValue} ${conversionInfo[i].measurement2} = ${conv2} ${conversionInfo[i].measurement1}</p>
+            </div>
+            `
+        }
     }
 })
